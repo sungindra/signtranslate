55aacfa65e16808c2c29b6ca1f6e5fc527914ad8
Sungindra Setiawan <sungindra.setiawan@gmail.com>
2020-06-06 11:08:11 +0700

    API structure adjustment

--- app/serializers/category_serializer.rb
+++ app/serializers/category_serializer.rb
@@ -9,15 +9,37 @@
 #  updated_at :datetime         not null
 #
 class CategorySerializer < ActiveModel::Serializer
-  attributes :id, :title, :image
-  has_many :sibi, serializer: SignDetailSerializer
-  has_many :bisindo, serializer: SignDetailSerializer
+  # attributes :id, :title, :image
+  # has_many :sibi, serializer: SignDetailSerializer
+  # has_many :bisindo, serializer: SignDetailSerializer
 
-  def sibi
-    object.signs.sibi
+  # def sibi
+  #   object.signs.sibi
+  # end
+
+  # def bisindo
+  #   object.signs.bisindo
+  # end
+
+  attributes :category, :sibiTitle, :sibiImage, :bisindoTitle, :bisindoImage
+
+  def category
+    object.title
   end
 
-  def bisindo
-    object.signs.bisindo
+  def sibiTitle
+    object.signs.sibi.pluck(:meaning)
+  end
+
+  def sibiImage
+    object.signs.sibi.pluck(:image)
+  end
+
+  def bisindoTitle
+    object.signs.bisindo.pluck(:meaning)
+  end
+
+  def bisindoImage
+    object.signs.bisindo.pluck(:image)
   end
 end
